aa634087bcb1ff20484412de5c23fffef866e6dd
Dylan RAMI <dylanrami.pro@outlook.com>
2026-09-04 15:41:58 +0200

    Apprentissage Python Etape 1 : Variables Types et Print

---
+++ semaine-2/Jour_1_Variables_Types_Print.py
@@ -0,0 +1,15 @@
+# Cours Variables,Types, print()
+
+airline = "Virgin Atlantic" #Str ou String qui désigne du texte
+registration = "GVYUM" #Str ou String qui désigne du texte
+price = 330.80 #Float qui désigne un nombre à virgule
+number_of_seats = 40 #Int ou Integer qui désigne un nombre entier
+aircraft_type = "Boieng 787-7 Dreamliner" # Str ou String qui désigne du texte
+full_flight = False #Bool ou Boolean pour deviner si c'est vrai ou faux
+
+print(airline)
+print(registration)
+print(price)
+print(number_of_seats)
+print(aircraft_type)
+print(full_flight)
